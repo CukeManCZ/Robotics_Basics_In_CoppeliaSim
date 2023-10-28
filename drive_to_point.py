a901b11e20f98c5aa2367b86a4cc8d0f8031489b
CukeManCZ <jindratraskos@gmail.com>
2023-10-28 09:08:04 +0200

    Change dependecy of drive_to_point.py to updated coppeliasim_zmqremoteapi_client, then rework main loop for more iterations based on user.

--- drive_to_point.py
+++ drive_to_point.py
@@ -1,57 +1,49 @@
 import math
-from Robotics_Basics_In_CoppeliaSim import sim
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 
-# Coppeliasim
-#   function create_shape(ints, floats, strings, bytes)
-#       sim.createPrimitiveShape(sim.primitiveshape_cuboid,{0.1,0.1,0.1},0)
-#       return {}, {}, {}, ''
-#   end
-#p = sim.simxCallScriptFunction(id, "/Generator", sim.sim_scripttype_childscript, "create_shape", {}, {}, {}, "", sim.simx_opmode_oneshot_wait)
-
-
-id = sim.simxStart("127.0.0.1", 19997, True, True, 5000, 5)
-
-if id < 0:
-    print("Connection failed.")
-    exit()
+client = RemoteAPIClient()
+sim = client.require("sim")
+sim.setStepping(True)
+sim.stopSimulation()
 
 print("Connected.")
 
-_, left_motor = sim.simxGetObjectHandle(id, "Left_drive", sim.simx_opmode_oneshot_wait)
-_, right_motor = sim.simxGetObjectHandle(id, "Right_drive", sim.simx_opmode_oneshot_wait)
-_, steering_motor = sim.simxGetObjectHandle(id, "Steering_drive", sim.simx_opmode_oneshot_wait)
+left_motor = sim.getObject("/Left_drive")
+right_motor = sim.getObject("/Right_drive")
+steering_motor = sim.getObject("/Steering_drive")
 
-_, goal_object = sim.simxGetObjectHandle(id, "Goal_point", sim.simx_opmode_oneshot_wait)
-_, robot_object = sim.simxGetObjectHandle(id, "Main_frame", sim.simx_opmode_oneshot_wait)
+goal_object = sim.getObject("/Goal_point")
+robot_object = sim.getObject("/Main_frame")
+robot_body = sim.getObject("/Robot")
 
 wheel_diameter = 0.1
 wheel_base = 0.3
 
 
 def get_xy(object_handle):
-    _, pos = sim.simxGetObjectPosition(id, object_handle, -1, sim.simx_opmode_oneshot_wait)
+    pos = sim.getObjectPosition(object_handle, sim.handle_world)
     return pos[0], pos[1]
 
 
 def compute_vel():
     kv = 0.7
-    goal_pos = get_xy(goal_object)
-    robot_pos = get_xy(robot_object)
+    ob1_pos = get_xy(goal_object)
+    ob2_pos = get_xy(robot_object)
 
-    distance = math.dist(goal_pos, robot_pos)
+    distance = math.dist(ob1_pos, ob2_pos)
 
     velocity = kv * distance
     return velocity
 
 
 def get_rel_angle():
-    goal_pos = get_xy(goal_object)
-    robot_pos = get_xy(robot_object)
-    return math.atan2(goal_pos[1]-robot_pos[1], goal_pos[0]-robot_pos[0], )
+    ob1_pos = get_xy(goal_object)
+    ob2_pos = get_xy(robot_object)
+    return math.atan2(ob1_pos[1]-ob2_pos[1], ob1_pos[0]-ob2_pos[0], )
 
 
 def get_yaw_angle(object_handle):
-    _, orientation = sim.simxGetObjectOrientation(id, object_handle, -1, sim.simx_opmode_oneshot_wait)
+    orientation = sim.getObjectOrientation(object_handle, sim.handle_world)
     return orientation[2]
 
 
@@ -68,28 +60,28 @@ def compute_steering():
     return kh * diff
 
 
-sim.simxStopSimulation(id, sim.simx_opmode_oneshot_wait)
+robot_start_pos = sim.getObjectPosition(robot_body, sim.handle_world)
+n_of_iterations = 5
 
-_, robot_start_pos = sim.simxGetObjectPosition(id, robot_object, -1, sim.simx_opmode_oneshot_wait)
-
-sim.simxSetObjectPosition(id, robot_object, -1, robot_start_pos, sim.simx_opmode_oneshot_wait)
-sim.simxStartSimulation(id, sim.simx_opmode_oneshot_wait)
-
-goal_pos = get_xy(goal_object)
-robot_pos = get_xy(robot_object)
-
-
-while math.dist(goal_pos, robot_pos) > 0.08:
-    v = compute_vel()
-    gamma = compute_steering()
-
-    sim.simxSetJointTargetVelocity(id, left_motor, v, sim.simx_opmode_oneshot_wait)
-    sim.simxSetJointTargetVelocity(id, right_motor, v, sim.simx_opmode_oneshot_wait)
-    sim.simxSetJointTargetPosition(id, steering_motor, gamma, sim.simx_opmode_oneshot_wait)
-
+sim.startSimulation()
+for iteration in range(1, n_of_iterations+1, 1):
     goal_pos = get_xy(goal_object)
     robot_pos = get_xy(robot_object)
 
-sim.simxPauseSimulation(id, sim.simx_opmode_oneshot_wait)
+    while math.dist(goal_pos, robot_pos) > 0.08:
+        v = compute_vel()
+        gamma = compute_steering()
 
+        sim.setJointTargetVelocity(left_motor, v)
+        sim.setJointTargetVelocity(right_motor, v)
+        sim.setJointTargetPosition(steering_motor, gamma)
 
+        goal_pos = get_xy(goal_object)
+        robot_pos = get_xy(robot_object)
+        sim.step()
+
+    sim.setObjectPosition(robot_body, robot_start_pos, sim.handle_world)
+    sim.wait(1)
+    print(iteration, ". iteration successful")
+
+sim.stopSimulation()
